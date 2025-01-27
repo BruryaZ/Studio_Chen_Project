a849e6fbacda34753d935eece2fcf23e35ad084b
Brurya-Zarbiv <bz0556758244@gmail.com>
2025-01-27 20:32:18 +0200

    Fix Unit test

---
+++ Studio_Chen.Data/MockData/FakeTeacherService.cs
@@ -0,0 +1,53 @@
+﻿using Studio_Chen.Core.Services;
+using Studio_Chen.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Studio_Chen.Data.MockData
+{
+    public class FakeTeacherService : ITeacherService
+    {
+        private readonly FakeData _fakeData;
+
+        public FakeTeacherService()
+        {
+            _fakeData = new FakeData();
+        }
+
+        public Task<IEnumerable<Teacher>> GetListAsync()
+        {
+            return Task.FromResult<IEnumerable<Teacher>>(_fakeData.LstTeacher);
+        }
+
+        public Task<Teacher> GetByIdAsync(int id)
+        {
+            var teacher = _fakeData.LstTeacher.FirstOrDefault(t => t.Identity == id.ToString());
+            return Task.FromResult(teacher);
+        }
+
+        public Task<Teacher> AddAsync(Teacher teacher)
+        {
+            _fakeData.LstTeacher.Add(teacher);
+            return Task.FromResult(teacher);
+        }
+
+        public Task<Teacher> UpdateAsync(int id, Teacher teacher)
+        {
+            var existingTeacher = _fakeData.LstTeacher.FirstOrDefault(t => t.Identity == id.ToString());
+            if (existingTeacher != null)
+            {
+                existingTeacher.FirstName = teacher.FirstName;
+                existingTeacher.LastName = teacher.LastName;
+                // עדכן שדות נוספים כנדרש
+            }
+            return Task.FromResult(existingTeacher);
+        }
+
+        public Task DeleteAsync(Teacher teacher)
+        {
+            _fakeData.LstTeacher.Remove(teacher);
+            return Task.CompletedTask;
+        }
+    }
+}
--- Studio_Chen.TEST/TeacherControllerTest.cs
+++ Studio_Chen.TEST/TeacherControllerTest.cs
@@ -1,59 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
 using Studio_Chen.API.Controllers;
-using Studio_Chen.Data;
 using Studio_Chen.Data.Models;
+using Studio_Chen.Core.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Studio_Chen.Data.MockData;
 
 namespace Studio_Chen.TEST
 {
     public class TeacherControllerTest
     {
-        //private readonly TeacherController _teacherController;
-        //public TeacherControllerTest()
-        //{
-        //    FakeData data = new FakeData();
-        //    _teacherController = new TeacherController();
-        //}
-        //[Fact]
-        //public void GetAll_ReturnsOk()
-        //{
-        //    //AAA
-        //    //Arrange
-        //    //Act
-        //    var result = _teacherController.Get();
-        //    //Assert
-        //    Assert.IsType<List<Teacher>>(result);
-        //}
-        //[Fact]
-        //public void GetAll_ReturnsCount()
-        //{
-        //    //AAA
-        //    //Arrange
-        //    //Act
-        //    var result = _teacherController.Get();
-        //    //Assert
-        //    Assert.Equal(2, result.Count());
-        //}
-        //[Fact]
-        //public void Get_ReturnsOk()
-        //{
-        //    //AAA
-        //    //Arrange
-        //    int id = 1;
-        //    //Act
-        //    var result = _teacherController.Get(id);
-        //    //Assert
-        //    Assert.IsType<OkObjectResult>(result);
-        //}
-        //[Fact]
-        //public void Get_NotFound()
-        //{
-        //    //AAA
-        //    //Arrange
-        //    int id = 100;
-        //    //Act
-        //    var result = _teacherController.Get(id);
-        //    //Assert
-        //    Assert.IsType<NotFoundResult>(result);
-        //}
+        private readonly TeacherController _teacherController;
+        private readonly ITeacherService _teacherService;
+
+        public TeacherControllerTest()
+        {
+            // כאן תוכל להוסיף Mock או Fake של ITeacherService
+            _teacherService = new FakeTeacherService(); // דוגמה, תחליף ביישום שלך
+            _teacherController = new TeacherController(_teacherService);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsOk()
+        {
+            //Arrange
+            //Act
+            var result = await _teacherController.GetAsync();
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var teachers = Assert.IsType<List<Teacher>>(okResult.Value);
+            Assert.NotNull(teachers);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsCount()
+        {
+            //Arrange
+            //Act
+            var result = await _teacherController.GetAsync();
+            //Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var teachers = Assert.IsType<List<Teacher>>(okResult.Value);
+            Assert.Equal(2, teachers.Count);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsOk()
+        {
+            //Arrange
+            int id = 1;
+            //Act
+            var result = await _teacherController.GetAsync(id);
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Get_NotFound()
+        {
+            //Arrange
+            int id = 100;
+            //Act
+            var result = await _teacherController.GetAsync(id);
+            //Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
     }
 }
